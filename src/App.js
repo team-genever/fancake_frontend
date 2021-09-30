2a3669178e9e12324af7284596bbc12d825bf4f8
jungminyoo <yoojm4718@gmail.com>
2021-10-01 00:03:21 +0900

    GlobalStyles 설정 - 폰트(Noto Sans KR) 및 box-sizing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from "react";
-import './App.css';
+// import "./App.css";
 
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
-import Error from './pages/Error';
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Wallet from "./pages/Wallet";
 
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import GlobalStyles from "./components/GlobalStyles";
 import Experience from "./pages/Experience";
 
 function App() {
   return (
-  <>
-    <Navbar />
-    <Switch>
-
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/experience" component={Experience}/>
-      <Route exact path="/rooms" component={Rooms}/>
-      <Route exact path="/rooms/:slug" component={SingleRoom}/>
-      <Route componenet={Error}/>
-    </Switch>
-  
-  </>
-  )
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/experience" component={Experience} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/wallet/:userId" component={Wallet} />
+        <Route component={Error} />
+      </Switch>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        margin: 0;
+        font-family: 'Noto Sans KR', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`;
+
+export default GlobalStyles;
