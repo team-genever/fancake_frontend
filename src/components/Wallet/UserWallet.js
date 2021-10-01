80c6029ee675274beac81d847fd39f85c3132245
jungminyoo <yoojm4718@gmail.com>
2021-10-02 04:49:59 +0900

    나의 지갑 페이지 충전하기 팝업 창

--- src/components/Wallet/UserWallet.js
+++ src/components/Wallet/UserWallet.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -102,8 +102,76 @@ const ChargeButton = styled.button`
   }
 `;
 
+const PopupBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundGray};
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PopupContainer = styled.div`
+  background-color: white;
+  width: 939px;
+  height: 597px;
+  padding: 70px 0 64px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  z-index: 11;
+  & h3 {
+    font-size: 40px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 48px;
+  }
+  & p {
+    font-weight: normal;
+    font-size: 32px;
+    max-width: 489px;
+    text-align: center;
+    margin-bottom: 35px;
+  }
+  & div {
+    display: flex;
+    justify-content: space-between;
+    width: 555px;
+  }
+  & small {
+    font-size: 28px;
+    font-weight: normal;
+    color: ${(props) => props.theme.boxGray};
+  }
+  & span {
+    font-size: 28px;
+    font-weight: normal;
+    color: black;
+  }
+  & button {
+    background-color: ${(props) => props.theme.mainPink};
+    color: white;
+    border: none;
+    padding: 20px 77px;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 10px;
+    margin-top: 40px;
+  }
+  & button:hover {
+    background-color: ${(props) => props.theme.mainPinkHover};
+    cursor: pointer;
+  }
+`;
+
 const UserWallet = () => {
   const { userId } = useParams();
+  const [popup, setPopup] = useState(false);
   return (
     <Container>
       <WalletTitle>
@@ -127,9 +195,42 @@ const UserWallet = () => {
             <span>거래내역</span>
           </HistoryButton>
         </Link>
-        <ChargeButton>
+        <ChargeButton
+          onClick={() => {
+            setPopup(true);
+          }}
+        >
           <span>충전하기</span>
         </ChargeButton>
+        {popup ? (
+          <PopupBackground>
+            <PopupContainer>
+              <h3>지갑 충전하기</h3>
+              <p>아래 계좌에 입금해주시면 3시간 이내 '내 지갑'에 들어옵니다.</p>
+              <div>
+                <small>은행</small>
+                <span>카카오뱅크</span>
+              </div>
+              <div>
+                <small>계좌번호</small>
+                <span>3333-02-0753215</span>
+              </div>
+              <div>
+                <small>예금주</small>
+                <span>백건우</span>
+              </div>
+              <button
+                onClick={() => {
+                  setPopup(false);
+                }}
+              >
+                확인했습니다
+              </button>
+            </PopupContainer>
+          </PopupBackground>
+        ) : (
+          <></>
+        )}
       </Buttons>
     </Container>
   );
