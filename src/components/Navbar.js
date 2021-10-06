04e285a4019c70d343e0ffb5faa62d5097591ff2
jungminyoo <yoojm4718@gmail.com>
2021-10-06 20:20:51 +0900

    네비게이션 바 파일 하나로 합침

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,126 @@
 import React from "react";
 import { withRouter } from "react-router";
-import NavBlack from "./NavBar/NavBlack";
-import NavWhite from "./NavBar/NavWhite";
+import logo_white from "images/logo_white.png";
+import logo_black from "images/logo_black.png";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Navbar = ({ location: { pathname } }) =>
-  pathname === "/" ? (
-    <NavBlack pathname={pathname} />
-  ) : (
-    <NavWhite pathname={pathname} />
+const Container = styled.div`
+  width: 100%;
+  height: 5vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 0 3vw;
+  background-color: ${(props) =>
+    props.isHome ? "rgba(0, 0, 0, 0.41)" : "white"};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid
+    ${(props) => (props.isHome ? props.theme.navBarUnderGray : "black")};
+`;
+
+const NavFront = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  & img {
+    height: 2.4vw;
+  }
+`;
+
+const HomeLink = styled(Link)`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 3vw;
+`;
+
+const SLink = styled(Link)`
+  height: 100%;
+  width: max-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  border-bottom: ${(props) =>
+    props.selected
+      ? `1px solid ${props.isHome ? props.theme.boxVeryLightGray : "black"}`
+      : "none"};
+  & span {
+    color: ${(props) =>
+      props.selected
+        ? props.isHome
+          ? props.theme.boxVeryLightGray
+          : "black"
+        : props.isHome
+        ? props.theme.boxLightGray
+        : props.theme.boxDarkGray};
+    font-size: 1vw;
+    font-weight: normal;
+  }
+  &:hover {
+    background-color: ${(props) =>
+      props.isHome ? "rgba(255, 255, 255, 0.3)" : props.theme.boxVeryLightGray};
+  }
+`;
+
+const Navigator = styled.div`
+  height: 100%;
+  display: flex;
+  gap: 1.5vw;
+`;
+
+const LoginButton = styled(Link)`
+  background-color: ${(props) => props.theme.mainPink};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12.5vw;
+  height: 3.3vw;
+  border-radius: 15px;
+  text-decoration: none;
+  & span {
+    color: white;
+    font-size: 1.1vw;
+    font-weight: bold;
+  }
+  &:hover {
+    background-color: ${(props) => props.theme.mainPinkHover};
+  }
+`;
+
+const Navbar = ({ location: { pathname } }) => {
+  const isHome = pathname === "/";
+  return (
+    <Container isHome={isHome}>
+      <NavFront>
+        <HomeLink to="/">
+          <img
+            src={isHome ? logo_white : logo_black}
+            alt={isHome ? "logo_white" : "logo_black"}
+          />
+        </HomeLink>
+        <Navigator>
+          <SLink to="/" selected={pathname === "/"} isHome={isHome}>
+            <span>서비스 소개</span>
+          </SLink>
+          <SLink
+            to="/experience"
+            selected={pathname === "/experience"}
+            isHome={isHome}
+          >
+            <span>체험하기</span>
+          </SLink>
+        </Navigator>
+      </NavFront>
+      <LoginButton to="/auth/main">
+        <span>로그인</span>
+      </LoginButton>
+    </Container>
   );
+};
 
 export default withRouter(Navbar);
